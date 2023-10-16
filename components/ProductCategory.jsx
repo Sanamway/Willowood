8a3388b7e58fcddeb7677aa52c10b0d95cebbf15
satish <satishwillowood@gmail.com>
2023-10-16 17:15:30 +0530

    product_category

--- components/ProductCategory.jsx
+++ components/ProductCategory.jsx
@@ -7,7 +7,7 @@ const ProductCategory = () => {
   const router = useRouter();
   return (
     <>
-    <Layout>
+      <Layout>
         <div className="h-screen overflow-auto w-full font-arial bg-white ">
           <div className="text-black flex items-center justify-between bg-white max-w-full font-arial h-[52px] px-5">
             <h2 className="font-arial font-normal text-3xl  py-2">Product Category </h2>
@@ -31,45 +31,35 @@ const ProductCategory = () => {
           <div className="text-black h-screen mb- ">
             <div className="bg-gray-100 p-4  h-sceen ">
               <form className="max-w-1/2 mx-4 mt mb-12 bg-white rounded shadow p-4">
-              
-                
-                <div className="flex -mx-2 mb-4">
-                  <div className="w-1/2 px-2">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="citySelect">
+                <div className="flex -mx-2 mb-4 flex-col">
+                  <div className="w-1/6 px-2 mb-2">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="inputField">
                       Category ID
                     </label>
-                    <select
-                    disabled={true}
-                      className="w-full px-3 bg-gray-100 py-2 border-b border-gray-500 rounded  focus:outline-none focus:border-b focus:border-indigo-500"
-                      id="citySelect"
-                    >
-                      {/* <option value="" className="focus:outline-none focus:border-b bg-white">
-                        Option
-                      </option> */}
-                      {/* <option value="city1">City 1</option>
-                      <option value="city2">City 2</option> */}
-                    </select>
+                    <input
+                      disabled
+                      className="w-full px-3 py-2 border rounded-lg border-gray-300 focus:outline-none focus:border-indigo-500"
+                      type="text"
+                      id="inputField"
+                      placeholder=""
+                    />
                   </div>
-                  <div className="w-1/2 px-2">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="stateSelect">
-                      <span className="text-red-500 p-1">*</span>Product Category
+                  <div className="w-1/2 px-2 ">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="inputField">
+                     <span className="text-red-500 px-1">*</span>Product Category
                     </label>
-                    <select
-                      className="w-full px-3 py-2 border-b border-gray-500 rounded bg-white focus:outline-none focus:border-b focus:border-indigo-500"
-                      id="stateSelect"
-                    >
-                      <option value="" className=" focus:outline-none focus:border-b bg-white">
-                        
-                      </option>
-                      <option value="state1">A</option>
-                      <option value="state2">B</option>
-                    </select>
+                    <input
+                      className="w-full px-3 py-2 border rounded-lg border-gray-300 focus:outline-none focus:border-indigo-500"
+                      type="text"
+                      id="inputField"
+                      placeholder="Input Product Category"
+                    />
                   </div>
                 </div>
                 <div className="flex -mx-2 mb-4">
                   <div className="w-1/2 px-2">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="userSelect">
-                    <span className="text-red-500 p-1">*</span>Company
+                      <span className="text-red-500 p-1">*</span>Company
                     </label>
                     <select
                       className="w-full px-3 py-2 border-b border-gray-500 rounded- bg-white focus:outline-none focus:border-b focus:border-indigo-500"
@@ -98,7 +88,6 @@ const ProductCategory = () => {
                     </select>
                   </div> */}
                 </div>
-        
 
                 {/* <button
                   className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
@@ -107,16 +96,16 @@ const ProductCategory = () => {
                   Submit
                 </button> */}
                 <div className="button flex items-center gap-3 mt-6">
-                <button className="bg-green-700 px-4 py-1 text-white">Save</button>
-                <button className="bg-yellow-500 px-4 py-1 text-white">Close</button>
-              </div>
+                  <button className="bg-green-700 px-4 py-1 text-white">Save</button>
+                  <button className="bg-yellow-500 px-4 py-1 text-white">Close</button>
+                </div>
               </form>
             </div>
           </div>
         </div>
       </Layout>
     </>
-  )
-}
+  );
+};
 
-export default ProductCategory
+export default ProductCategory;
