1804da8aac68b74488b0b75287cc8cbd3fce761b
satish <satishwillowood@gmail.com>
2023-10-16 16:31:45 +0530

    assign role

---
+++ pages/form/assign_role.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import AssignRole from '@/components/AssignRole'
+const assign_role = () => {
+  return (
+    <>
+    <AssignRole></AssignRole>
+    </>
+  )
+}
+
+export default assign_role
