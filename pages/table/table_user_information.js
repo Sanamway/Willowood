e91f30c9d17b482aa3f095499526f1e0de018cb0
satish <satishwillowood@gmail.com>
2023-10-16 14:54:24 +0530

    userinfomartio form

---
+++ pages/table/table_user_information.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import UserInformation from '@/components/UserInformation'
+const table_user_information = () => {
+  return (
+    <>
+    <UserInformation></UserInformation>
+    </>
+  )
+}
+
+export default table_user_information
