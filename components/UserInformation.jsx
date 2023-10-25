1fe6435591d3b91b7074c44661c5d7bd75d07fc1
satish <satishwillowood@gmail.com>
2023-10-25 16:06:40 +0530

    user information, profile pic,desig,password fields updated

--- components/UserInformation.jsx
+++ components/UserInformation.jsx
@@ -3,6 +3,8 @@ import Layout from "./Layout";
 import { AiTwotoneHome } from "react-icons/ai";
 import { TiArrowBack } from "react-icons/ti";
 import { useRouter } from "next/router";
+import userimg from "../public/userimg.jpg";
+import Image from "next/image";
 const UserInformation = () => {
   const router = useRouter();
   const [formData, setFormData] = useState({
@@ -31,6 +33,17 @@ const UserInformation = () => {
     console.log(formData);
   };
 
+  const [userImage, setUserImage] = useState(null);
+
+  // Function to handle image upload
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      
+      setUserImage(URL.createObjectURL(file));
+    }
+  };
+
   return (
     <>
       <Layout>
@@ -49,10 +62,12 @@ const UserInformation = () => {
               </h2>
               <h2>
                 <AiTwotoneHome
-                onClick={() => {
-                  router.push("/");
-                }} 
-                 className="text-red-500" size={34}></AiTwotoneHome>
+                  onClick={() => {
+                    router.push("/");
+                  }}
+                  className="text-red-500"
+                  size={34}
+                ></AiTwotoneHome>
               </h2>
             </div>
           </div>
@@ -61,15 +76,74 @@ const UserInformation = () => {
           <div className="text-black h-screen mb- ">
             <div className="bg-gray-100 p-4  ">
               <form onSubmit={handleSubmit} className="max-w-1/2 mx-4 mt mb-12 bg-white rounded shadow p-4">
-                <div className="mb-4">
+                <div className="flex items-center justify-between w-full">
+                  {/* <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="inputField">
+                      User Name
+                    </label>
+                    <input
+                      className="w-full px-3 py-2 border rounded-lg border-gray-300 focus:outline-none focus:border-indigo-500"
+                      type="text"
+                      id="inputField"
+                      placeholder="Username"
+                    />
+                  </div> */}
+                  <div className="flex gap-4 items-start justify-between mb-4 w-3/4">
+                    <div className="w-1/2 ">
+                      <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="emailField">
+                        Employee
+                      </label>
+                      <input
+                        className="w-full px-3 py-2 border rounded-lg border-gray-300 focus:outline-none focus:border-indigo-500"
+                        type="text"
+                        id="inputField"
+                        placeholder="Employee Code"
+                      />
+                    </div>
+                    <div className="w-1/2">
+                      <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phoneField">
+                        User name
+                      </label>
+                      <input
+                        className="w-full px-3 py-2 border rounded-lg border-gray-300 focus:outline-none focus:border-indigo-500"
+                        type="text"
+                        id="inputField"
+                        placeholder="Username"
+                      />
+                    </div>
+                  </div>
+                  <div className="profpic relative group">
+                    <Image
+                      src={userImage ? userImage : userimg}
+                      className="h-32 w-32 rounded-full"
+                      alt="Profile"
+                      width={100}
+                    />
+                    <input
+                      type="file"
+                      accept="image/*"
+                      onChange={handleImageUpload}
+                      style={{ display: "none" }}
+                      id="fileInput"
+                    />
+                    <label
+                      htmlFor="fileInput"
+                      className="text-orange-500 absolute font-semibold top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+                    >
+                      Edit
+                    </label>
+                  </div>
+                </div>
+
+                <div className="mb-4 designation">
                   <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="inputField">
-                    User Name
+                    Designation
                   </label>
                   <input
                     className="w-full px-3 py-2 border rounded-lg border-gray-300 focus:outline-none focus:border-indigo-500"
                     type="text"
                     id="inputField"
-                    placeholder="Username"
+                    placeholder="Designation"
                   />
                 </div>
                 <div className="mb-1">
@@ -77,7 +151,7 @@ const UserInformation = () => {
                     Address
                   </label>
                   <textarea
-                  rows={4}
+                    rows={4}
                     className="w-full px-3 py-1.5  border rounded-lg border-gray-300 focus:outline-none focus:border-indigo-500"
                     id="textareaField"
                     placeholder="Address"
@@ -89,7 +163,7 @@ const UserInformation = () => {
                       City
                     </label>
                     <select
-                      className="w-full px-3 py-2 border-b border-gray-500 rounded-md bg-white focus:outline-none focus:border-b focus:border-indigo-500"
+                      className="w-full px-3 py-2 border-b border-gray-500 bg-white focus:outline-none focus:border-b focus:border-indigo-500"
                       id="citySelect"
                     >
                       <option value="" className="focus:outline-none focus:border-b bg-white">
@@ -104,7 +178,7 @@ const UserInformation = () => {
                       State
                     </label>
                     <select
-                      className="w-full px-3 py-2 border-b border-gray-500 rounded-md bg-white focus:outline-none focus:border-b focus:border-indigo-500"
+                      className="w-full px-3 py-2 border-b border-gray-500  bg-white focus:outline-none focus:border-b focus:border-indigo-500"
                       id="stateSelect"
                     >
                       <option value="" className="focus:outline-none focus:border-b bg-white">
@@ -115,7 +189,7 @@ const UserInformation = () => {
                     </select>
                   </div>
                 </div>
-               
+
                 <div className="flex gap-4 items-center justify-between mb-4">
                   <div className="w-1/2">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="emailField">
@@ -140,13 +214,38 @@ const UserInformation = () => {
                     />
                   </div>
                 </div>
+
+                <div className="flex gap-4 items-center justify-between mb-4">
+                  <div className="w-1/2">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="emailField">
+                      Password
+                    </label>
+                    <input
+                      className="w-full px-3 py-2 border rounded-lg border-gray-300 focus:outline-none focus:border-indigo-500"
+                      type="password"
+                      id="passwordField"
+                      placeholder="Password"
+                    />
+                  </div>
+                  <div className="w-1/2">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phoneField">
+                      Confirm Password
+                    </label>
+                    <input
+                      className="w-full px-3 py-2 border rounded-lg border-gray-300 focus:outline-none focus:border-indigo-500"
+                      type="password"
+                      id="confirmPass"
+                      placeholder="Confirm Password"
+                    />
+                  </div>
+                </div>
                 <div className="flex -mx-2 mb-4">
                   <div className="w-1/2 px-2">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="userSelect">
                       User Profile
                     </label>
                     <select
-                      className="w-full px-3 py-2 border-b border-gray-500 rounded-md bg-white focus:outline-none focus:border-b focus:border-indigo-500"
+                      className="w-full px-3 py-2 border-b border-gray-500  bg-white focus:outline-none focus:border-b focus:border-indigo-500"
                       id="userSelect"
                     >
                       <option value="" className="focus:outline-none focus:border-b bg-white">
@@ -158,10 +257,10 @@ const UserInformation = () => {
                   </div>
                   <div className="w-1/2 px-2">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="statusSelect">
-                       Status
+                      Status
                     </label>
                     <select
-                      className="w-full px-3 py-2 border-b border-gray-500 rounded-md bg-white focus:outline-none focus:border-b focus:border-gray-500"
+                      className="w-full px-3 py-2 border-b border-gray-500  bg-white focus:outline-none focus:border-b focus:border-gray-500"
                       id="statusSelect"
                     >
                       <option value="" className="focus:outline-none focus:border-b bg-white">
@@ -173,18 +272,29 @@ const UserInformation = () => {
                   </div>
                 </div>
 
-                {/* <button
-                  className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                  type="submit"
-                >
-                  Submit
-                </button> */}
+                <div className="mb-1">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="textareaField">
+                    About Me
+                  </label>
+                  <textarea
+                    rows={5}
+                    className="w-full px-3 py-1.5  border rounded-lg border-gray-300 focus:outline-none focus:border-indigo-500"
+                    id="textareaField"
+                    placeholder="About"
+                  ></textarea>
+                </div>
+
                 <div className="button flex items-center gap-3 mt-6">
-                <button className="bg-green-700 px-4 py-1 text-white">Save</button>
-                <button onClick={() => {
-                    router.push("/table/table_user_information");
-                  }} className="bg-yellow-500 px-4 py-1 text-white">Close</button>
-              </div>
+                  <button className="bg-green-700 px-4 py-1 text-white">Save</button>
+                  <button
+                    onClick={() => {
+                      router.push("/table/table_user_information");
+                    }}
+                    className="bg-yellow-500 px-4 py-1 text-white"
+                  >
+                    Close
+                  </button>
+                </div>
               </form>
             </div>
           </div>
