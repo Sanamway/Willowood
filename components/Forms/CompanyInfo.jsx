09294a2105185f80c33637a57f1ecd4369f87fcb
Sanam <sanamwaymehta@willowood.com>
2023-10-31 10:58:21 +0530

    New

--- components/Forms/CompanyInfo.jsx
+++ components/Forms/CompanyInfo.jsx
@@ -1,42 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../Layout";
-import { AiFillFileExcel, AiTwotoneHome } from "react-icons/ai";
+import { AiTwotoneHome } from "react-icons/ai";
 import { TiArrowBack } from "react-icons/ti";
 import axios from "axios";
 import { url } from "@/constants/url";
 import { useRouter } from "next/router";
 const CompanyInfo = () => {
   const router = useRouter();
-  
+
   const headers = {
     "Content-Type": "application/json",
-    secret: "fsdhfgsfuiweifiowefjewcewcebjw"
+    secret: "fsdhfgsfuiweifiowefjewcewcebjw",
   };
 
-  const gettingMenuRole = async () => {
+  const handleSaveCompanyInfo = async () => {
+    const data = {
+      cmpny_name: companyState.companyName,
+      corp_address: companyState.corpAdress,
+      corp_address_city: companyState.corpAddressCity,
+      corp_address_state: companyState.corpAddressState,
+      sale_address: companyState.saleAddress,
+      sale_address_city: companyState.saleAddressCity,
+      sale_address_state: companyState.saleAddressState,
+      email: companyState.email,
+      phone_number: companyState.phoneNumber,
+      contact_person: companyState.contactPerson,
+      gst_no: companyState.gstNum,
+      c_name: companyState.companyName,
+      ul_name: "Ultimate Leader",
+    };
     try {
-      const respond = await axios.get(`${url}/get_company_informations`, { headers: headers });
-      const apires = await respond.data.data;
-      setMenusRole(apires);
+      const respond = await axios
+        .post(`${url}/api/add_company_informations`, JSON.stringify(data), {
+          headers: headers,
+        })
+        .then((res) => {
+          window.alert(res.me);
+          router.push("/table/table_company_info");
+        });
     } catch (error) {
-      console.log(error);
+      window.alert(error);
     }
   };
 
-  const gettingMenuName = async () => {
-    try {
-      const respond = await axios.get(`${url}/api/menus`, { headers: headers });
-      const apires = await respond.data.data;
-      setMenusName(apires);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const [companyState, setCompanyState] = useState({
+    companyName: "",
+    corpAdress: "",
+    corpAddressCity: "",
+    corpAddressState: "",
+    saleAddress: "",
+    saleAddressCity: "",
+    saleAddressState: "",
+    email: "",
+    phoneNumber: "",
+    contactPerson: "",
+    gstNum: "",
+    cName: "",
+    ulName: "",
+  });
 
-  useEffect(() => {
-    gettingMenuRole();
-    // gettingMenuName();
-  }, []);
+  const handleDatat = (e) => {
+    const { name, value } = e.target;
+    console.log(value);
+  };
 
   return (
     <Layout>
@@ -68,7 +94,10 @@ const CompanyInfo = () => {
         </div>
 
         <div className="bg-gray-0 p-4 bg-gray-100  w-full flex items-start ">
-          <form className=" bg-white rounded shadow p-4 w-full mb-8 "  onSubmit={(e) => e.preventDefault()}>
+          <form
+            className=" bg-white rounded shadow p-4 w-full mb-8 "
+            onSubmit={(e) => e.preventDefault()}
+          >
             <div className="mb-4 w-1/6">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
@@ -95,6 +124,13 @@ const CompanyInfo = () => {
                 type="text"
                 id="inputField"
                 placeholder="Company Name"
+                value={companyState.companyName}
+                onChange={(e) =>
+                  setCompanyState({
+                    ...companyState,
+                    companyName: e.target.value,
+                  })
+                }
               />
             </div>
             <div className="flex w-full justify-between gap-4">
@@ -110,6 +146,13 @@ const CompanyInfo = () => {
                   id="textareaField"
                   placeholder="Corporate Address"
                   rows="6"
+                  value={companyState.corpAdress}
+                  onChange={(e) =>
+                    setCompanyState({
+                      ...companyState,
+                      corpAdress: e.target.value,
+                    })
+                  }
                 ></textarea>
               </div>
               <div className="w-full">
@@ -124,6 +167,13 @@ const CompanyInfo = () => {
                   id="textareaField"
                   rows="6"
                   placeholder="Sale Address"
+                  value={companyState.saleAddress}
+                  onChange={(e) =>
+                    setCompanyState({
+                      ...companyState,
+                      saleAddress: e.target.value,
+                    })
+                  }
                 ></textarea>
               </div>
             </div>
@@ -141,12 +191,18 @@ const CompanyInfo = () => {
                     <select
                       className="w-full px-3 py-2 border-b border-gray-500 rounded-md bg-white focus:outline-none focus:border-b focus:border-indigo-500"
                       id="userSelect"
+                      onClick={(e) =>
+                        setCompanyState({
+                          ...companyState,
+                          corpAddressCity: e.target.value,
+                        })
+                      }
                     >
                       <option
                         value=""
                         className="focus:outline-none focus:border-b bg-white"
                       >
-                        Option
+                        City
                       </option>
                       <option value="user1">User 1</option>
                       <option value="user2">User 2</option>
@@ -162,12 +218,18 @@ const CompanyInfo = () => {
                     <select
                       className="w-full px-3 py-2 border-b border-gray-500 rounded-md bg-white focus:outline-none focus:border-b focus:border-indigo-500"
                       id="userSelect"
+                      onClick={(e) =>
+                        setCompanyState({
+                          ...companyState,
+                          corpAddressState: e.target.value,
+                        })
+                      }
                     >
                       <option
                         value=""
                         className="focus:outline-none focus:border-b bg-white"
                       >
-                        Option
+                        State
                       </option>
                       <option value="user1">User 1</option>
                       <option value="user2">User 2</option>
@@ -189,12 +251,18 @@ const CompanyInfo = () => {
                     <select
                       className="w-full px-3 py-2 border-b border-gray-500 rounded-md bg-white focus:outline-none focus:border-b focus:border-indigo-500"
                       id="userSelect"
+                      onClick={(e) =>
+                        setCompanyState({
+                          ...companyState,
+                          saleAddressCity: e.target.value,
+                        })
+                      }
                     >
                       <option
                         value=""
                         className="focus:outline-none focus:border-b bg-white"
                       >
-                        Option
+                        City
                       </option>
                       <option value="user1">User 1</option>
                       <option value="user2">User 2</option>
@@ -210,12 +278,18 @@ const CompanyInfo = () => {
                     <select
                       className="w-full px-3 py-2 border-b border-gray-500 rounded-md bg-white focus:outline-none focus:border-b focus:border-indigo-500"
                       id="userSelect"
+                      onClick={(e) =>
+                        setCompanyState({
+                          ...companyState,
+                          saleAddressState: e.target.value,
+                        })
+                      }
                     >
                       <option
                         value=""
                         className="focus:outline-none focus:border-b bg-white"
                       >
-                        Option
+                        State
                       </option>
                       <option value="user1">User 1</option>
                       <option value="user2">User 2</option>
@@ -238,6 +312,13 @@ const CompanyInfo = () => {
                   type="text"
                   id="inputField"
                   placeholder="Mobile"
+                  value={companyState.phoneNumber}
+                  onChange={(e) =>
+                    setCompanyState({
+                      ...companyState,
+                      phoneNumber: e.target.value,
+                    })
+                  }
                 />
               </div>
               <div className="w-full">
@@ -252,6 +333,13 @@ const CompanyInfo = () => {
                   type="email"
                   id="inputField"
                   placeholder="Email"
+                  value={companyState.email}
+                  onChange={(e) =>
+                    setCompanyState({
+                      ...companyState,
+                      email: e.target.value,
+                    })
+                  }
                 />
               </div>
             </div>
@@ -267,6 +355,13 @@ const CompanyInfo = () => {
                 type="text"
                 id="inputField"
                 placeholder="Contact Person"
+                value={companyState.contactPerson}
+                onChange={(e) =>
+                  setCompanyState({
+                    ...companyState,
+                    contactPerson: e.target.value,
+                  })
+                }
               />
             </div>
             <div className="w-1/2">
@@ -281,12 +376,24 @@ const CompanyInfo = () => {
                 type="text"
                 id="inputField"
                 placeholder="GST Number"
+                value={companyState.gstNum}
+                onChange={(e) =>
+                  setCompanyState({
+                    ...companyState,
+                    gstNum: e.target.value,
+                  })
+                }
               />
             </div>
             <div className="button flex items-center gap-3 mt-6">
-              <div className="bg-green-700 px-4 py-1 text-white">Save</div>
+              <div
+                className="bg-green-700 px-4 py-1 text-white cursor-pointer"
+                onClick={() => handleSaveCompanyInfo()}
+              >
+                Save
+              </div>
               <button
-                className="bg-yellow-500 px-4 py-1 text-white"
+                className="bg-yellow-500 px-4 py-1 text-white cursor-pointer"
                 onClick={() => {
                   router.push("/table/table_company_info");
                 }}
--- components/Table/CompanyInfo.jsx
+++ components/Table/CompanyInfo.jsx
@@ -3,62 +3,46 @@ import Layout from "../Layout";
 import { AiTwotoneHome } from "react-icons/ai";
 import { TiArrowBack } from "react-icons/ti";
 import { useRouter } from "next/router";
+import axios from "axios";
+import { url } from "@/constants/url";
 import { AiOutlineSearch } from "react-icons/ai";
 const CompanyInfo = () => {
   const router = useRouter();
-  const newFunc = async () => {
-    const url = "192.168.126:3005/API/get_company_information";
-    const response = await fetch(url, {
-      method: "GET", // *GET, POST, PUT, DELETE, etc.
-      mode: "cors", // no-cors, *cors, same-origin
-      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: "same-origin", // include, *same-origin, omit
-      headers: {
-        "Content-Type": "application/json",
-        secret: "fsdhfgsfuiweifiowefjewcewcebjw",
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      redirect: "follow", // manual, *follow, error
-      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    });
-    return response.json(); // parses JSON response into native JavaScript objects
-  };
-  useEffect(() => {
-    newFunc();
-  }, []);
-
   const [data, setData] = useState(null);
+  const headers = {
+    "Content-Type": "application/json",
+    secret: "fsdhfgsfuiweifiowefjewcewcebjw",
+  };
+  const getCompanyInfo = async () => {
+    try {
+      const respond = await axios.get(`${url}/api/get_company_information`, {
+        headers: headers,
+      });
+      const apires = await respond.data.data;
+      setData(apires);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    const fetchDataWithHeaders = async () => {
-      const url = "192.168.126:3005/API/get_company_information";
-
-      // Create a Headers object and set your headers
-      const headers = new Headers();
-      headers.append("Authorization", "Bearer yourAccessToken");
-      headers.append("Content-Type", "application/json");
-      headers.append("secret", "fsdhfgsfuiweifiowefjewcewcebjw");
-      try {
-        const response = await fetch(url, {
-          method: "GET", // You can use 'POST', 'PUT', 'DELETE', etc. based on your request type
-          headers: headers, // Set the headers here
-        });
-
-        if (response.ok) {
-          const responseData = await response.json();
-          setData(responseData);
-        } else {
-          // Handle the error
-          console.error("Request failed with status:", response.status);
-        }
-      } catch (error) {
-        // Handle any other errors, e.g., network issues
-        console.error("Error:", error);
-      }
-    };
-
-    fetchDataWithHeaders();
+    getCompanyInfo();
   }, []);
+  const handleDeleteCompany = async (id, name) => {
+    if (window.confirm(`Delete ${name} !!`))
+      try {
+        const respond = await axios.delete(
+          `${url}/api/delete_company_information/${id}`,
+          {
+            headers: headers,
+          }
+        ).then((res)=> getCompanyInfo());
+        const apires = await respond.data.data;
+        setData(apires);
+      } catch (error) {
+        console.log(error);
+      }
+  };
   return (
     <Layout>
       <div className="h-screen overflow-auto w-full font-arial bg-white ">
@@ -133,54 +117,56 @@ const CompanyInfo = () => {
                 </tr>
               </thead>
               <tbody className="bg-white divide-y  divide-gray-200 text-xs">
-                <tr className="dark:border-2">
-                  <td className="px-4 py-2 text-left dark:border-2 whitespace-nowrap font-arial text-xs ">
-                    <button
-                      onClick={() => {
-                        router.push("/form/company_info_form");
-                      }}
-                      className="b text-black   hover:text-blue-500  "
-                    >
-                      View
-                    </button>
-                    <button
-                      onClick={() => {
-                        router.push("/form/company_info_form");
-                      }}
-                      className="b text-black hover:text-yellow-400 ml-2"
-                    >
-                      Edit
-                    </button>
-                    <button className="b text-black hover:text-red-500 ml-2">
-                      Delete
-                    </button>
-                  </td>
-                  <td className="px-4 py-2 dark:border-2 whitespace-nowrap">
-                    2
-                  </td>
-                  <td className="px-4 py-2 dark:border-2 whitespace-nowrap">
-                    Company B
-                  </td>
+                {data?.map((item, idx) => (
+                  <tr className="dark:border-2">
+                    <td className="px-4 py-2 text-left dark:border-2 whitespace-nowrap font-arial text-xs ">
+                      <button
+                        onClick={() => {
+                          router.push("/form/company_info_form");
+                        }}
+                        className="b text-black   hover:text-blue-500  "
+                      >
+                        View
+                      </button>
+                      <button className="b text-black hover:text-yellow-400 ml-2">
+                        Edit
+                      </button>
+                      <button
+                        className="b text-black hover:text-red-500 ml-2"
+                        onClick={() => {
+                          handleDeleteCompany(item._id, item.cmpny_name);
+                        }}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                    <td className="px-4 py-2 dark:border-2 whitespace-nowrap">
+                      {idx+1}
+                    </td>
+                    <td className="px-4 py-2 dark:border-2 whitespace-nowrap">
+                      {item.cmpny_name}
+                    </td>
 
-                  <td className="px-4 py-2 dark:border-2 whitespace-nowrap">
-                    3
-                  </td>
-                  <td className="px-4 py-2 dark:border-2 whitespace-nowrap">
-                    Company C
-                  </td>
-                  <td className="px-4 py-2 dark:border-2 whitespace-nowrap">
-                    3
-                  </td>
-                  <td className="px-4 py-2 dark:border-2 whitespace-nowrap">
-                    Company C
-                  </td>
-                  <td className="px-4 py-2 dark:border-2 whitespace-nowrap">
-                    3
-                  </td>
-                  <td className="px-4 py-2 dark:border-2 whitespace-nowrap">
-                    3
-                  </td>
-                </tr>
+                    <td className="px-4 py-2 dark:border-2 whitespace-nowrap">
+                      {item.corp_address}
+                    </td>
+                    <td className="px-4 py-2 dark:border-2 whitespace-nowrap">
+                      {item.sale_address}
+                    </td>
+                    <td className="px-4 py-2 dark:border-2 whitespace-nowrap">
+                      {item.contact_person}
+                    </td>
+                    <td className="px-4 py-2 dark:border-2 whitespace-nowrap">
+                      {item.phone_number}
+                    </td>
+                    <td className="px-4 py-2 dark:border-2 whitespace-nowrap">
+                      {item.email}
+                    </td>
+                    <td className="px-4 py-2 dark:border-2 whitespace-nowrap">
+                      {item.gst_no}
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
