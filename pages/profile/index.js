d1af5077ad42e26fddee2814de46594b2b13e2a4
satish <satishwillowood@gmail.com>
2023-12-16 12:23:25 +0530

    User Profile BST Details API Integratation done

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import UserProfile from '@/components/UserProfile'
 import UserProfile from '@/components/forms/UserProfile'
 import Layout from '@/components/Layout'
 const index = () => {
