599f825d3fbbbdffff1d736a5c357b4a7310adc7
satish <satishwillowood@gmail.com>
2023-12-26 12:43:07 +0530

    chartreports design as per UA

---
+++ pages/chartreports/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Topbar from '@/components/RPTransaction/Topbar'
+const index = () => {
+  return (
+    <>
+    <Topbar></Topbar>
+    </>
+  )
+}
+
+export default index
